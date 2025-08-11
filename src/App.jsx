b9728230103d625ab4738a97081ba245d153aab8
Fahad-smto <simanto.smto.prog@gmail.com>
2025-08-12 02:24:01 +0600

    project is ready to create

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-3xl bg-amber-300'>portfolio web</h1>
+       
+    </>
+  )
+}
+
+export default App
